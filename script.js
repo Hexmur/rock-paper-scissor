9a978af7d5fdda031cca0aff7cc28c526e687724
Hexmur <mauroalizzi@gmail.com>
2025-12-02 19:25:54 +0100

    implemented getComChoice() and getPlayerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("ciao");
+function getComChoice(){
+  let result = Math.floor(Math.random()*3);
+  let comMove = "";
+  if(result === 0) comMove = "rock";
+  else if(result === 1) comMove = "paper";
+  else comMove = "scissor";
+  return comMove;
+}
+
+function getPlayerChoice(){
+  let playerMove = prompt("What's your move?");
+  return playerMove;
+}
+
+console.log(getPlayerChoice());
+console.log(getComChoice());
