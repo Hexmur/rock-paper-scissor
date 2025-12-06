75a09ef229a02b01766285488863cf663c4ee7e6
Hexmur <mauroalizzi@gmail.com>
2025-12-06 13:15:30 +0100

    Implemented UI

--- script.js
+++ script.js
@@ -42,25 +42,55 @@ function playRound(playerChoice, comChoice){
 }
 
 function playGame(){
-
-  let playerScore = 0;
-  let comScore = 0;
-  let roundResult = 0;
-
-  for(i=0; i<5; i++){
-    console.log(`Round ${i+1}`);
-    let playerChoice = getPlayerChoice();
-    let comChoice = getComChoice();
-    roundResult = playRound(playerChoice, comChoice);
-    if(roundResult === 1) playerScore++;
-    else if(roundResult === 0) comScore++;
-  }
-
-  if(playerScore > comScore) console.log("You win the match!");
-  else if(playerScore < comScore) console.log("You lose the match!");
-  else console.log(`The result of the match is a tie!`);
-
   console.log(`Player ${playerScore} - Com ${comScore}`);
 }
+
+const btnRock = document.querySelector("#btnRock");
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissor = document.querySelector("#btnScissor");
+
+const divBtn = document.querySelector("#divBtn");
+const divResult = document.querySelector("#divResult");
+
+const playerResultText = document.querySelector("#playerResult");
+const comResultText = document.querySelector("#comResult");
+const winnerText = document.querySelector("#winner");
+
+let playerScore = 0;
+let comScore = 0;
+let roundResult = 0;
+let round = 0;
+let playerChoice = "";
+
+divBtn.addEventListener("click", (event) =>{
+
+  switch(event.target.id){
+    case 'btnRock': playerChoice = "rock"; break;
+    case 'btnPaper': playerChoice = "paper"; break;
+    case 'btnScissor': playerChoice = "scissor"; break;
+    case 'divBtn': console.log("Please press a button"); return;
+  }
+
+  console.log(`Round ${round+1}`);
+  let comChoice = getComChoice();
+  roundResult = playRound(playerChoice, comChoice);
+  if(roundResult === 1){
+    playerScore++;
+    playerResultText.textContent = playerScore;
+  }
+  else if(roundResult === 0){
+    comScore++;
+    comResultText.textContent = comScore;
+  }
+
+  if(playerScore === 5){
+    winnerText.textContent = "You are the winner!"
+  }
+
+  else if(playerScore === 5){
+    winnerText.textContent = "You lose the game!"
+  }
+
+});
 
 playGame();
